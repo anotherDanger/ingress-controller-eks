2dba55aa95f10114ac2f5b832a7abe48537b25ff
Andhika Danger <andhikadanger666@gmail.com>
2025-11-08 23:29:57 +0700

    add new vpc to each cluster and new sg to each k6 instance

--- main.tf
+++ main.tf
@@ -17,16 +17,41 @@ provider "aws" {
   region = var.aws_region
 }
 
-module "vpc" {
+module "vpc_nginx" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.5.2"
 
-  name = "eks-vpc"
+  name = "eks-vpc-nginx"
   cidr = "10.0.0.0/16"
 
   azs             = ["ap-southeast-1a", "ap-southeast-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+module "vpc_traefik" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.5.2"
+
+  name = "eks-vpc-traefik"
+  cidr = "10.1.0.0/16"
+
+  azs             = ["ap-southeast-1a", "ap-southeast-1b"]
+  private_subnets = ["10.1.1.0/24", "10.1.2.0/24"]
+  public_subnets  = ["10.1.101.0/24", "10.1.102.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
@@ -49,15 +74,15 @@ module "eks_nginx" {
   cluster_name    = "eks-nginx"
   cluster_version = "1.29"
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc_nginx.vpc_id
+  subnet_ids = module.vpc_nginx.private_subnets
 
   cluster_endpoint_public_access = true
 
   eks_managed_node_groups = {
     eks_node_group = {
       instance_types = ["m7i-flex.large"]
-      ami_type = "AL2023_x86_64_STANDARD"
+      ami_type       = "AL2023_x86_64_STANDARD"
       min_size       = 1
       max_size       = 1
       desired_size   = 1
@@ -67,7 +92,7 @@ module "eks_nginx" {
     },
     ingress_node_group = {
       instance_types = ["t3.small"]
-      ami_type = "AL2023_x86_64_STANDARD"
+      ami_type       = "AL2023_x86_64_STANDARD"
       min_size       = 1
       max_size       = 1
       desired_size   = 1
@@ -92,15 +117,15 @@ module "eks_traefik" {
   cluster_name    = "eks-traefik"
   cluster_version = "1.29"
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc_traefik.vpc_id
+  subnet_ids = module.vpc_traefik.private_subnets
 
   cluster_endpoint_public_access = true
 
   eks_managed_node_groups = {
     eks_node_group = {
       instance_types = ["m7i-flex.large"]
-      ami_type = "AL2023_x86_64_STANDARD"
+      ami_type       = "AL2023_x86_64_STANDARD"
       min_size       = 1
       max_size       = 1
       desired_size   = 1
@@ -110,7 +135,7 @@ module "eks_traefik" {
     },
     ingress_node_group = {
       instance_types = ["t3.small"]
-      ami_type = "AL2023_x86_64_STANDARD"
+      ami_type       = "AL2023_x86_64_STANDARD"
       min_size       = 1
       max_size       = 1
       desired_size   = 1
@@ -128,10 +153,10 @@ module "eks_traefik" {
   }
 }
 
-resource "aws_security_group" "k6_instance_sg" {
-  name        = "k6-instance-sg"
-  description = "Allow SSH inbound traffic for k6 instance"
-  vpc_id      = module.vpc.vpc_id
+resource "aws_security_group" "k6_nginx_sg" {
+  name        = "k6-nginx-sg"
+  description = "Allow SSH inbound traffic for k6 NGINX instance"
+  vpc_id      = module.vpc_nginx.vpc_id
 
   ingress {
     from_port   = 22
@@ -148,17 +173,41 @@ resource "aws_security_group" "k6_instance_sg" {
   }
 
   tags = {
-    Name = "k6-instance-sg"
+    Name = "k6-nginx-sg"
+  }
+}
+
+resource "aws_security_group" "k6_traefik_sg" {
+  name        = "k6-traefik-sg"
+  description = "Allow SSH inbound traffic for k6 Traefik instance"
+  vpc_id      = module.vpc_traefik.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "k6-traefik-sg"
   }
 }
 
 resource "aws_instance" "k6_nginx" {
   ami                         = "ami-0933f1385008d33c4"
   instance_type               = "m7i-flex.large"
-  subnet_id                   = module.vpc.public_subnets[0]
+  subnet_id                   = module.vpc_nginx.public_subnets[0]
   key_name                    = "your-key"
   associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.k6_instance_sg.id]
+  vpc_security_group_ids      = [aws_security_group.k6_nginx_sg.id]
 
   tags = {
     Name = "k6-nginx"
@@ -168,10 +217,10 @@ resource "aws_instance" "k6_nginx" {
 resource "aws_instance" "k6_traefik" {
   ami                         = "ami-0933f1385008d33c4"
   instance_type               = "m7i-flex.large"
-  subnet_id                   = module.vpc.public_subnets[1]
+  subnet_id                   = module.vpc_traefik.public_subnets[0]
   key_name                    = "your-key"
   associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.k6_instance_sg.id]
+  vpc_security_group_ids      = [aws_security_group.k6_traefik_sg.id]
 
   tags = {
     Name = "k6-traefik"
